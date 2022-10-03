b86bfa08c6b7231f658fb51371a4784ad7aa9312
NecomaV <73179504+NecomaV@users.noreply.github.com>
2022-10-03 20:05:17 +0600

    4 path 3 codelab  7. Adopt good coding practices  full

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,19 +11,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener{
-            rollDice()
-        }
+        //listener for the button when pushed
+        rollButton.setOnClickListener{ rollDice() }
     }
-
+    // rollDice function
     private fun rollDice() {
         var dice = Dice(6)
         var diceRoll = dice.roll()
+        //finds a textview
         var resultTextView : TextView= findViewById(R.id.textView)
+        //makes textview equal the value from diceroll
         resultTextView.text = diceRoll.toString()
     }
 }
 class Dice(val numSides : Int){
+    //randomizer between 1 and sides that we give
     fun roll() : Int{
         return(1..numSides).random()
     }
