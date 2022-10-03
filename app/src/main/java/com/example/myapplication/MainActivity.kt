ffabf7d0ee2d946643bb00bd9dc4231d9fed0d49
NecomaV <73179504+NecomaV@users.noreply.github.com>
2022-10-03 19:38:17 +0600

    4 path 3 codelab  5. Make the Button interactive  2 3 4 5

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,11 +3,16 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener{
+            val toast = Toast.makeText(this,"Dice Rolled", Toast.LENGTH_SHORT)
+            toast.show()
+        }
     }
 }
