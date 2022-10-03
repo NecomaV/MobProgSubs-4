6ffbe2d338e957878d7776fe040cdb683487bacf
NecomaV <73179504+NecomaV@users.noreply.github.com>
2022-10-03 20:26:29 +0600

    4 path 5 codelab 6. Adopt good coding practices

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -19,7 +19,13 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         var dice = Dice(6)
         var diceRoll = dice.roll()
+
+
+        // Find the ImageView in the layout
         val diceImage: ImageView = findViewById(R.id.imageView)
+
+
+        // Determine which drawable resource ID to use based on the dice roll
         val drawableResource = when (diceRoll) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
@@ -31,7 +37,8 @@ class MainActivity : AppCompatActivity() {
         }
         diceImage.setImageResource(drawableResource)
 
-
+        // Update the content description
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 class Dice(val numSides : Int){
