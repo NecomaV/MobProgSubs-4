0c3d50def5f1d181a8211b5f3093a8d0b01e27d9
NecomaV <73179504+NecomaV@users.noreply.github.com>
2022-10-03 21:36:29 +0600

    5 path 1 4. Build the layout in XML

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,55 +11,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.button)
-        //listener for the button when pushed
-        rollButton.setOnClickListener{ rollDice() }
-    }
-    // rollDice function
-    private fun rollDice() {
-        var dice = Dice(6)
-        var diceRoll = dice.roll()
-        var diceRoll2 = dice.roll2()
 
-
-        // Find the ImageView in the layout
-        val diceImage: ImageView = findViewById(R.id.imageView)
-        val diceImage2: ImageView = findViewById(R.id.imageView2)
-
-
-        // Determine which drawable resource ID to use based on the dice roll
-        val drawableResource = when (diceRoll) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-
-            else -> R.drawable.dice_6
-        }
-        val drawableResource2 = when (diceRoll2) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-
-            else -> R.drawable.dice_6
-        }
-
-        diceImage.setImageResource(drawableResource)
-        diceImage2.setImageResource(drawableResource2)
-
-        // Update the content description
-        diceImage.contentDescription = diceRoll.toString()
-    }
-}
-class Dice(val numSides : Int){
-    //randomizer between 1 and sides that we give
-    fun roll() : Int{
-        return(1..numSides).random()
-    }
-    fun roll2() : Int{
-        return(1..numSides).random()
     }
 }
